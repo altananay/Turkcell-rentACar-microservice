d346dc8115c95840d64ef1b59ca2af8a25b4601f
altananay <altananay@outlook.com>
2023-09-16 01:42:40 +0300

    Prometheus, Actuator, Grafana, Zipkin added.

--- common-package/src/main/java/com/kodlamaio/commonpackage/security/SecurityConfig.java
+++ common-package/src/main/java/com/kodlamaio/commonpackage/security/SecurityConfig.java
@@ -8,18 +8,36 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
-
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
-    {
-        var converter = new JwtAuthenticationConverter();
-        converter.setJwtGrantedAuthoritiesConverter(new KeycloakJwtRoleConverter());
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    var converter = new JwtAuthenticationConverter();
+    converter.setJwtGrantedAuthoritiesConverter(new KeycloakJwtRoleConverter());
 
-        http.cors().and().authorizeHttpRequests().requestMatchers("/api/filters", "/api/cars/check-car-available/**", "/api/payments/check", "/api/cars").permitAll().requestMatchers("/api/**").hasAnyRole("user").anyRequest().authenticated().and().csrf().disable().oauth2ResourceServer().jwt().jwtAuthenticationConverter(converter);
+    http.cors()
+        .and()
+        .authorizeHttpRequests()
+        .requestMatchers(
+            "/api/filters",
+            "/api/cars/check-car-available/**",
+            "/api/payments/check",
+            "/api/cars",
+            "/api/cars/**",
+            "/actuator/**")
+        .permitAll()
+        .requestMatchers("/api/**")
+        .hasAnyRole("user")
+        .anyRequest()
+        .authenticated()
+        .and()
+        .csrf()
+        .disable()
+        .oauth2ResourceServer()
+        .jwt()
+        .jwtAuthenticationConverter(converter);
 
-        return http.build();
-    }
+    return http.build();
+  }
 }
